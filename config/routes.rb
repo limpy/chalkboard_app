a373f1e486b38421b331a5bfb8402535fb1da742
Kevin Curtin <kevincurtin88@gmail.com>
2011-10-15 12:37:28 -0700

    Added pages controller and a user controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 ChalkboardApp::Application.routes.draw do
-  get "pages/home"
+  
+  root :to => 'pages#home' 
+  match '/about', :to => 'pages#about'
 
-  get "pages/contact"
+  match '/contact', :to => 'pages#contact'
 
-  get "pages/about"
-
-  get "pages/help"
+  match '/help', :to => 'pages#help'
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
